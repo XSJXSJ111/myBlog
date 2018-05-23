bfd1ff79ba9f13a261b61f9d71835e0101db2324
XSJXSJ <970369982@qq.com>
2018-05-23 11:05:36 +0800

    测试

--- sw.js
+++ sw.js
@@ -1,49 +1,46 @@
-// 当前缓存版本的唯一标识符，用当前时间代替
-var cacheKey = 'v52';
-// 当前缓存的白名单，在新脚本的 install 事件里将使用白名单里的 key 
-var cacheWhitelist = [cacheKey];
-// 需要被缓存的文件的 URL 列表 
-var cacheFileList = global.serviceWorkerOption.assets;
-console.log('缓存列表',cacheFileList)
+var VERSION = 'v26';
 
-// 监听 install 事件
-self.addEventListener('install', function (event) {
+// 缓存
+self.addEventListener('install', function(event) {
   console.log('安装')
-  // 等所有资源缓存完成时，才可以进行下一步 
   event.waitUntil(
-    caches.open(cacheKey).then(function (cache) {
-      // 要缓存的文件 URL 列表
-      return cache.addAll(cacheFileList);
-    }));
-});
-// 拦截网络请求
-self.addEventListener('fetch', function (event) {
-  console.log('网络请求')
-  event.respondWith(
-    // 去缓存中查询对应的请求 
-    caches.match(event.request).then(function (response) {
-      // 如果命中本地缓存，就直接返回本地的资源 
-      if (response) {
-        return response;
-      }
-      // 否则就用 fetch 下载资源 
-      return fetch(event.request);
+    caches.open(VERSION).then(function(cache) {
+      return cache.addAll([
+        './start.html',
+        './jquery-2.2.4.min.js',
+        './mm1.jpg'
+      ]);
     })
   );
 });
 
-// 新的 Service Workers 线程取得控制权后，将会触发 activate 事件
-self.addEventListener('activate', function (event) {
-  console.log('激活')
+// 缓存更新
+self.addEventListener('activate', function(event) {
+  console.log('注册')
   event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(cacheNames.map(function (cacheName) {
-        // 将不在白名单中的缓存全部清理掉
-        if (cacheWhitelist.indexOf(cacheName) === -1) {
-          // 删除缓存
-          console.log('删除')
-          return caches.delete(cacheName);
-        }
-      }));
-    }));
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          // 如果当前版本和缓存版本不一致
+          if (cacheName !== VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 捕获请求并返回缓存数据
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(response) {
+    caches.open(VERSION).then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('./mm1.jpg');
+  }));
 });
